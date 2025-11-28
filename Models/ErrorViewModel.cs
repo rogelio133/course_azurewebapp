5f0d0aaa556a60e254215166ecb0be1a1c414a3a
Rogelio Guillermo Pacheco Elorza <rogelio_133@outlook.com>
2025-11-28 12:46:28 -0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace platziwebsite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
